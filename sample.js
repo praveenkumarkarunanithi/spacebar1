3e88e35f30f192549c689282660d7ab47064f357
praveenkumarkarunanithi <100338903+praveenkumarkarunanithi@users.noreply.github.com>
2022-08-10 11:01:29 +0530

    Update sample.js

--- sample.js
+++ sample.js
@@ -6,6 +6,9 @@ var grid = new ej.grids.Grid({
     editSettings: { allowEditing: true, allowAdding: true, allowDeleting: true},
     filterSettings: { type: 'Excel' },
     toolbar: [{ text: 'Search'}, 'Add', 'Edit', 'Delete', 'Update', 'Cancel'],
+    keyPressed: function () {
+        debugger;
+    },
     columns: [
       {field: 'OrderID',headerText: 'Order ID',width: 120,isPrimaryKey: true},
       { field: 'CustomerID', headerText: 'Customer Name', width: 150 },
